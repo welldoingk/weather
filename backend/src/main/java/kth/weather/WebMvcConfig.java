d3e9b3f5a931b78c8c927530b0a721f0279bcac0
welldoingk <thkwon96@gmail.com>
2023-05-28 12:11:16 +0900

    리엑트용 cors 설정

---
+++ backend/src/main/java/kth/weather/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package kth.weather;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+}
